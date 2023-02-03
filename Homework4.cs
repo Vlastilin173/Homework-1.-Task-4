20f41c84afea89f162bc90146e3c6fe75a94ee22
Vladimir <vova_spirid@mail.ru>
2023-02-04 02:35:57 +0400

    pull request

---
+++ Homework4.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число: ");
+int o = Convert.ToInt32(Console.ReadLine());
+if (n > m ) 
+    if (n > o)
+    Console.WriteLine (n);
+    else  
+    Console.WriteLine(o); 
+else if (m > o)   
+    Console.WriteLine (m);
+else 
+    Console.WriteLine (o);
+
